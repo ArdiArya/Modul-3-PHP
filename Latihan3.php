4f2a8551d5f51f9c5f92c2f36c008f418e4782bc
ardi_arya_27rpl <ardi_arya_27rpl@student.smktelkom-mlg.sch.id>
2019-02-22 10:41:35 +0700

    hasil

---
+++ Latihan3.php
@@ -0,0 +1,8 @@
+<?php 
+    $user = "ArdiArya"; $pass = "1411";
+    if ($user == "ArdiArya" && $pass == "1411") { 
+        echo "Login Berhasil"; 
+    } else { 
+        echo "Login Gagal"; 
+    } 
+?>  
---
+++ Praktikum.php
@@ -0,0 +1,9 @@
+<?php
+ $user = $_POST["username"]; 
+ $pass = $_POST["password"]; 
+    if ($user == "ArdiArya" && $pass == "1411") {     
+            echo "Login Berhasil"; 
+        } else {     
+            echo "Login Gagal";
+        } 
+?> 
